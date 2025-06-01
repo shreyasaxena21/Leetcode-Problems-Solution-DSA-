08b066e8a350dde0a998be3ce956cadbdd3aea3d
Shreya Saxena <172243056+shreyasaxena21@users.noreply.github.com>
2025-06-01 05:37:27 +0000

    java

---
+++ 169. Majority Element.java
@@ -0,0 +1,35 @@
+class Solution {
+    //approch 1 - Brute Force
+    public int majorityElement1(int[] nums) {
+        int n = nums.length;
+        for(int i= 0; i<nums.length; i++){
+            int count = 0;
+            for(int j = 0; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+
+            }
+            if(count>n/2){
+                return nums[i];
+            }
+
+        }
+        return -1;
+    }
+    //approach 2 - Hashing
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+
+            if(map.get(nums[i])>n/2){
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+
+}
