2e2f037406bcf239541a7da7cd3e5c46c723ae60
Shreya Saxena <172243056+shreyasaxena21@users.noreply.github.com>
2025-06-03 13:03:08 +0000

    java

---
+++ 56. Merge Intervals.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> result = new ArrayList<>();
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
+        for(int [] interval : intervals){
+            if(result.isEmpty() || result.get(result.size()-1)[1]<interval[0]){
+                result.add(interval); //startTime
+            }
+            else{
+                result.get(result.size()-1)[1] = Math.max(result.get(result.size()-1)[1], interval[1]);
+            }
+        }
+        return result.toArray(new int[result.size()][]); //list coverted into 2 d array
+    }
+}
