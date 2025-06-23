58be79d91994bf716928cf99aed7bc5fb8c562ba
Shreya Saxena <172243056+shreyasaxena21@users.noreply.github.com>
2025-06-23 15:34:19 +0000

    java

---
+++ 389. Find the Difference.java
@@ -0,0 +1,15 @@
+class Solution {
+ 
+
+     public char findTheDifference(String s, String t) {
+        char xor = 0;
+        for(char c : s.toCharArray()){
+            xor = (char)(xor ^ c);
+        }
+        for(char c : t.toCharArray()){
+            xor ^= c;
+        }
+
+        return xor;
+     }
+}
