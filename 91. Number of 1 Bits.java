d14e6442b2551aa1243dea9726be043ecb0f3161
Shreya Saxena <172243056+shreyasaxena21@users.noreply.github.com>
2025-06-23 08:53:32 +0000

    java

--- 91. Number of 1 Bits.java
+++ 91. Number of 1 Bits.java
@@ -1,5 +1,18 @@
 class Solution {
-    public int hammingWeight(int n) {
+    public int hammingWeight1(int n) {
+        //approach 1
         return Integer.bitCount(n);   
     }
+
+     public int hammingWeight(int n) {
+        //approach 2
+        int count = 0;
+        while(n>0){
+            n = n & (n-1);
+            count++;
+        }
+
+        return count;
+
+     }
 }
